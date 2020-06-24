710f1bfc6f481fec3936fbcd21453b9805d6881c
admin <admin@my.io>
2020-06-25 00:21:14 +0800

    增加测试数据

--- pipeline/service.py
+++ pipeline/service.py
@@ -2,6 +2,7 @@ from .model import db, Graph, Vertex, Edge
 from .utils import get_logger
 from .config import LOGS_DIR
 from functools import wraps
+import json
 
 logger = get_logger(__name__, '{}/{}.log'.format(LOGS_DIR, __name__))
 
@@ -16,6 +17,7 @@ def transaction(fn):
             return ret
         except Exception as e:
             db.session.rollback()
+            raise e
     return wrapper
 
 
@@ -31,12 +33,12 @@ def create_graph(name, desc=None):
 
 # 为DAG增加顶点
 @transaction
-def add_vertex(graph: Graph, name: str, script: str = None, in_put: str = None):
+def add_vertex(graph: Graph, name: str, in_put: str = None, script: str = None):
     vertex = Vertex()
     vertex.graph_id = graph.id
     vertex.name = name
-    vertex.script = script
     vertex.input = in_put
+    vertex.script = script
     db.session.add(vertex)
     return vertex
 
@@ -63,6 +65,79 @@ def del_vertex(v_id):
         # 再删除顶点
         query.delete()
     return v
+
+
+def test_create_dag():
+    try:
+        # 创建DAG
+        g = create_graph('test1')  # 返回一个Graph对象
+
+        # 增加顶点
+        in_put = """
+        {
+            "ip":{
+                "type": "str",
+                "required": true,
+                "default":"192.168.0.100"
+            }
+        }
+        """
+        script = {
+            "script":"echo test1.A",
+            "next": "B"
+        }  # script中next表示下一节点，可以是下一节点的id，也可以是名称
+
+        a = add_vertex(g, 'A', None, json.dumps(script))
+        b = add_vertex(g, 'B', None, 'echo B')
+        c = add_vertex(g, 'C', None, 'echo C')
+        d = add_vertex(g, 'D', None, 'echo D')
+
+        # 增加边
+        ab = add_edge(g, a, b)
+        ac = add_edge(g, a, c)
+        cb = add_edge(g, c, b)
+        bd = add_edge(g, b, d)
+
+        # 创建一个环图
+        g = create_graph('test2')
+        # 增加顶点
+        a = add_vertex(g, 'A', None, 'echo A')
+        b = add_vertex(g, 'B', None, 'echo B')
+        c = add_vertex(g, 'C', None, 'echo C')
+        d = add_vertex(g, 'D', None, 'echo D')
+        # 增加边，abc形成一个环
+        ba = add_edge(g, b, a)
+        ac = add_edge(g, a, c)
+        cb = add_edge(g, c, b)
+        bd = add_edge(g, b, d)
+
+        # 创建多个终点的DAG
+        g = create_graph('test3')
+        # 增加顶点
+        a = add_vertex(g, 'A', None, 'echo A')
+        b = add_vertex(g, 'B', None, 'echo B')
+        c = add_vertex(g, 'C', None, 'echo C')
+        d = add_vertex(g, 'D', None, 'echo D')
+        # 增加边
+        ba = add_edge(g, b, a)
+        ac = add_edge(g, a, c)
+        bc = add_edge(g, b, c)
+        bd = add_edge(g, b, d)
+
+        # 创建一个有多个入口的DAG
+        g = create_graph('test4')
+        # 增加顶点
+        a = add_vertex(g, 'A', None, 'echo A')
+        b = add_vertex(g, 'B', None, 'echo B')
+        c = add_vertex(g, 'C', None, 'echo C')
+        d = add_vertex(g, 'D', None, 'echo D')
+        # 增加边
+        ab = add_edge(g, a, b)
+        ac = add_edge(g, a, c)
+        cb = add_edge(g, c, b)
+        db = add_edge(g, d, b)
+    except Exception as e:
+        print(e)
 
 
 
